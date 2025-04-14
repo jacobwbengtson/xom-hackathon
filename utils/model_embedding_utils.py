47f33729b984610e52f8f7a2a2d4ef92bcab5908
Jake <jacobwbengtson@gmail.com>
2025-04-13 19:01:18 -0600

    Update model_embedding_utils.py
    removed hard coded enpoints

--- utils/model_embedding_utils.py
+++ utils/model_embedding_utils.py
@@ -11,10 +11,10 @@ def _handle_caii_request(sentence: str):
   API_KEY = json.load(open("/tmp/jwt"))["access_token"]
 
   # Set "query" in MODEL_ID for user questions to find matches in the vector store. Use "passage" when embedding reference data into the vector store.
-  MODEL_ID = "nvidia/nv-embedqa-e5-v5-passage"
+  MODEL_ID = os.environ.get("EMBEDDING_MODEL_ID")
 
   client = OpenAI(
-    base_url="https://ml-2dad9e26-62f.go01-dem.ylcu-atmi.cloudera.site/namespaces/serving-default/endpoints/goes---e5-embeddings/v1",
+    base_url=os.environ.get("EMBEDDING_MODEL_ENDPOINT"),
     api_key=API_KEY,
   )
 
